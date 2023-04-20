830099d91fec048f7fc3fe8d4fba48e562ad9c4b
Minhyeonhong <mhh2029@gmail.com>
2023-04-20 13:02:17 +0900

    test : pre-render test

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <App />
-);
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <App />
+// );
+
+import { hydrate, render } from "react-dom";
+
+const rootElement = document.getElementById("root");
+if (rootElement!.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
